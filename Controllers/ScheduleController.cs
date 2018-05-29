9c90ae719c5440f3f38232bb69f1fdb90dc5845e
ELVIN-PC\elvin <0907984@hr.nl>
2018-05-29 15:49:42 +0200

    first commit

---
+++ Controllers/ScheduleController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace InfoView.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        // 
+        // GET: /Groups/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Groups/Welcome/  
+        public IActionResult Rooms(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        } 
+
+        
+
+    }
+}
